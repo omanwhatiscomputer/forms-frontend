ff5431d5d9bb224e94dcf8a67f0b7bdf4eab9b3c
omanwhatiscomputer <khubayeebkabeer@gmail.com>
2025-01-15 02:02:28 +0600

    post-def-commit-8

--- app/(components)/MarkdownInput.js
+++ app/(components)/MarkdownInput.js
@@ -1,147 +1,159 @@
+/* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
 
-import React, { useState, useCallback, useMemo } from "react";
-import Prism from "prismjs";
-import "prismjs/components/prism-markdown";
-import {
-    createEditor,
-    Editor,
-    Element as SlateElement,
-    Node as SlateNode,
-    Text,
-} from "slate";
-import { withHistory } from "slate-history";
-import { Editable, ReactEditor, Slate, withReact } from "slate-react";
-import { SHORTCUTS } from "./slatejs/markdown/constants";
-import { Element, Leaf, withShortcuts } from "./slatejs/markdown/editor";
-import CustomPlaceholder from "./slatejs/CustomPlaceholder";
+// import React, { useState, useCallback, useMemo } from "react";
+// import Prism from "prismjs";
+// import "prismjs/components/prism-markdown";
+// import {
+//     createEditor,
+//     Editor,
+//     Element as SlateElement,
+//     Node as SlateNode,
+//     Text,
+// } from "slate";
+// import { withHistory } from "slate-history";
+// import { Editable, ReactEditor, Slate, withReact } from "slate-react";
+// import { SHORTCUTS } from "./slatejs/markdown/constants";
+// import { Element, Leaf, withShortcuts } from "./slatejs/markdown/editor";
+// import CustomPlaceholder from "./slatejs/CustomPlaceholder";
 import { useDispatch, useSelector } from "react-redux";
-import { markdownDefault } from "@/constants/defaultValue";
+// import { markdownDefault } from "@/constants/defaultValue";
 import { selectFormMode } from "@/lib/features/form/formSlice";
 import { formMode } from "@/constants/formMode";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const MarkdownInput = ({ placeholder, className, action, value, formId }) => {
     const dispatch = useDispatch();
     const mode = useSelector((state) => selectFormMode(state, formId));
-    const renderElement = useCallback((props) => <Element {...props} />, []);
-    const editor = useMemo(
-        () => withShortcuts(withReact(withHistory(createEditor()))),
-        []
-    );
-    const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+    // const renderElement = useCallback((props) => <Element {...props} />, []);
+    // const editor = useMemo(
+    //     () => withShortcuts(withReact(withHistory(createEditor()))),
+    //     []
+    // );
+    // const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
 
-    const handleDOMBeforeInput = useCallback(() => {
-        queueMicrotask(() => {
-            const pendingDiffs = ReactEditor.androidPendingDiffs(editor);
-            const scheduleFlush = pendingDiffs?.some(({ diff, path }) => {
-                if (!diff.text.endsWith(" ")) {
-                    return false;
-                }
-                const { text } = SlateNode.leaf(editor, path);
-                const beforeText =
-                    text.slice(0, diff.start) + diff.text.slice(0, -1);
-                if (!(beforeText in SHORTCUTS)) {
-                    return;
-                }
-                const blockEntry = Editor.above(editor, {
-                    at: path,
-                    match: (n) =>
-                        SlateElement.isElement(n) && Editor.isBlock(editor, n),
-                });
-                if (!blockEntry) {
-                    return false;
-                }
-                const [, blockPath] = blockEntry;
-                return Editor.isStart(
-                    editor,
-                    Editor.start(editor, path),
-                    blockPath
-                );
-            });
-            if (scheduleFlush) {
-                ReactEditor.androidScheduleFlush(editor);
-            }
-        });
-    }, [editor]);
+    // const handleDOMBeforeInput = useCallback(() => {
+    //     queueMicrotask(() => {
+    //         const pendingDiffs = ReactEditor.androidPendingDiffs(editor);
+    //         const scheduleFlush = pendingDiffs?.some(({ diff, path }) => {
+    //             if (!diff.text.endsWith(" ")) {
+    //                 return false;
+    //             }
+    //             const { text } = SlateNode.leaf(editor, path);
+    //             const beforeText =
+    //                 text.slice(0, diff.start) + diff.text.slice(0, -1);
+    //             if (!(beforeText in SHORTCUTS)) {
+    //                 return;
+    //             }
+    //             const blockEntry = Editor.above(editor, {
+    //                 at: path,
+    //                 match: (n) =>
+    //                     SlateElement.isElement(n) && Editor.isBlock(editor, n),
+    //             });
+    //             if (!blockEntry) {
+    //                 return false;
+    //             }
+    //             const [, blockPath] = blockEntry;
+    //             return Editor.isStart(
+    //                 editor,
+    //                 Editor.start(editor, path),
+    //                 blockPath
+    //             );
+    //         });
+    //         if (scheduleFlush) {
+    //             ReactEditor.androidScheduleFlush(editor);
+    //         }
+    //     });
+    // }, [editor]);
 
-    const decorate = useCallback(([node, path]) => {
-        const ranges = [];
+    // const decorate = useCallback(([node, path]) => {
+    //     const ranges = [];
 
-        if (!Text.isText(node)) {
-            return ranges;
-        }
+    //     if (!Text.isText(node)) {
+    //         return ranges;
+    //     }
 
-        const getLength = (token) => {
-            if (typeof token === "string") {
-                return token.length;
-            } else if (typeof token.content === "string") {
-                return token.content.length;
-            } else {
-                return token.content.reduce((l, t) => l + getLength(t), 0);
-            }
-        };
-        const tokens = Prism.tokenize(node.text, Prism.languages.markdown);
-        let start = 0;
-        for (const token of tokens) {
-            const length = getLength(token);
-            const end = start + length;
-            if (typeof token !== "string") {
-                ranges.push({
-                    [token.type]: true,
-                    anchor: { path, offset: start },
-                    focus: { path, offset: end },
-                });
-            }
-            start = end;
-        }
-        return ranges;
-    }, []);
+    //     const getLength = (token) => {
+    //         if (typeof token === "string") {
+    //             return token.length;
+    //         } else if (typeof token.content === "string") {
+    //             return token.content.length;
+    //         } else {
+    //             return token.content.reduce((l, t) => l + getLength(t), 0);
+    //         }
+    //     };
+    //     const tokens = Prism.tokenize(node.text, Prism.languages.markdown);
+    //     let start = 0;
+    //     for (const token of tokens) {
+    //         const length = getLength(token);
+    //         const end = start + length;
+    //         if (typeof token !== "string") {
+    //             ranges.push({
+    //                 [token.type]: true,
+    //                 anchor: { path, offset: start },
+    //                 focus: { path, offset: end },
+    //             });
+    //         }
+    //         start = end;
+    //     }
+    //     return ranges;
+    // }, []);
 
-    const [isFocused, setIsFocused] = useState(false);
+    // const [isFocused, setIsFocused] = useState(false);
     const editorStyle = `p-2 pl-4 border-foreground focus:border-primary focus:ring-0 focus:outline-none ${className}`;
     return (
-        <Slate
-            editor={editor}
-            initialValue={value}
-            onChange={(v) => dispatch(action({ value: v, id: formId }))}
-        >
-            <Editable
-                {...((mode === formMode.readonly ||
-                    mode === formMode.respond) && { readOnly: true })}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                decorate={decorate}
-                renderLeaf={renderLeaf}
-                onDOMBeforeInput={handleDOMBeforeInput}
-                renderElement={renderElement}
-                spellCheck
-                className={`${editorStyle}`}
+        // <Slate
+        //     editor={editor}
+        //     initialValue={value}
+        //     onChange={(v) => dispatch(action({ value: v, id: formId }))}
+        // >
+        //     <Editable
+        //         {...((mode === formMode.readonly ||
+        //             mode === formMode.respond) && { readOnly: true })}
+        //         onFocus={() => setIsFocused(true)}
+        //         onBlur={() => setIsFocused(false)}
+        //         decorate={decorate}
+        //         renderLeaf={renderLeaf}
+        //         onDOMBeforeInput={handleDOMBeforeInput}
+        //         renderElement={renderElement}
+        //         spellCheck
+        //         className={`${editorStyle}`}
+        //     />
+        //     {mode !== formMode.readonly &&
+        //         mode !== formMode.respond &&
+        //         (editor.children.length === 0 ||
+        //             JSON.stringify(editor.children) ===
+        //                 JSON.stringify(markdownDefault)) &&
+        //         !isFocused && (
+        //             <CustomPlaceholder
+        //                 isFocused={isFocused}
+        //                 className="pl-4 -translate-y-[32px] h-0"
+        //             >
+        //                 <p>{placeholder || "Enter text here..."}</p>
+        //             </CustomPlaceholder>
+        //         )}
+        // </Slate>
+        <div>
+            <textarea
+                className={`${editorStyle} ${
+                    (mode !== formMode.readonly || mode !== formMode.respond) &&
+                    "hidden"
+                }`}
+                type="text"
+                rows="3"
+                value={value}
+                onChange={(e) =>
+                    dispatch(action({ value: e.target.value, id: formId }))
+                }
+                placeholder="Enter form description"
             />
-            {mode !== formMode.readonly &&
-                mode !== formMode.respond &&
-                (editor.children.length === 0 ||
-                    JSON.stringify(editor.children) ===
-                        JSON.stringify(markdownDefault)) &&
-                !isFocused && (
-                    <CustomPlaceholder
-                        isFocused={isFocused}
-                        className="pl-4 -translate-y-[32px] h-0"
-                    >
-                        <p>{placeholder || "Enter text here..."}</p>
-                    </CustomPlaceholder>
-                )}
-        </Slate>
+
+            <Markdown remarkPlugins={[remarkGfm]} className={editorStyle}>
+                {value}
+            </Markdown>
+        </div>
     );
 };
 
-// const initialValue = [
-//     {
-//         type: "paragraph",
-//         children: [
-//             {
-//                 text: "",
-//             },
-//         ],
-//     },
-// ];
 export default MarkdownInput;
